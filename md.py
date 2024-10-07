3f210bf444e746ceaf417e4e4f54546323b0a1a3
@KiKi_BS <156508809+kiki9402@users.noreply.github.com>
2024-10-07 22:28:02 +0900

    Update md.py

--- md.py
+++ md.py
@@ -4,11 +4,11 @@ from bs4 import BeautifulSoup
 def hangang_data():
     url = "https://hangang.life"
     res = requests.get(url)
-    res.raise_for_status()  # 요청이 성공했는지 확인
+    res.raise_for_status()#요청
 
     soup = BeautifulSoup(res.text, "lxml")
-    temp = soup.find("span", attrs={"class": "bold"})  # 한강온도
-    bold2 = soup.find("span",attrs={"class": "bold", "style": "color: rgba(255, 255, 255, 0.9);"})  # 날짜
+    temp = soup.find("span", attrs={"class": "bold"})
+    bold2 = soup.find("span",attrs={"class": "bold", "style": "color: rgba(255, 255, 255, 0.9);"})
 
     return {
         "temperature": temp.get_text(),
